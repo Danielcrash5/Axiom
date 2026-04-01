76ae3fcd4ba088b35a99c9d4bf238e48df8dbd32
Danielcrash5 <daniel.vekony@gmx.de>
2026-04-01 11:22:13 +0200

    etwas übersehen in Window.cpp

--- axiom/src/platform/Window.cpp
+++ axiom/src/platform/Window.cpp
@@ -36,7 +36,6 @@ namespace axiom {
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-		glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
 
 		// Fenster erstellen
