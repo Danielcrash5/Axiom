fcbccd98a8a9f53928fa62c04f1f08973c4b9a2e
Danielcrash5 <daniel.vekony@gmx.de>
2026-04-12 15:58:45 +0200

    Neue Entity funktionen

--- axiom/include/axiom/ecs/Entity.h
+++ axiom/include/axiom/ecs/Entity.h
@@ -5,41 +5,52 @@
 namespace axiom {
 
 
-    class Entity {
-    public:
-        Entity() = default;
-        Entity(entt::entity handle, Scene* scene)
-            : m_Entity(handle), m_Scene(scene) {
-        }
-
-        void Destroy() {
-            m_Scene->m_Registry.destroy(m_Entity);
+	class Entity {
+	public:
+		Entity() = default;
+		Entity(entt::entity handle, Scene* scene)
+			: m_Entity(handle), m_Scene(scene) {
 		}
 
-        template<typename T, typename... Args>
-        T& AddComponent(Args&&... args) {
-            return m_Scene->m_Registry.emplace<T>(
-                m_Entity, std::forward<Args>(args)...);
-        }
+		void Destroy() {
+			m_Scene->m_Registry.destroy(m_Entity);
+		}
 
-        template<typename T>
-        T& GetComponent() {
-            return m_Scene->m_Registry.get<T>(m_Entity);
-        }
+		template<typename T, typename... Args>
+		T& AddComponent(Args&&... args) {
+			return m_Scene->m_Registry.emplace<T>(
+				m_Entity, std::forward<Args>(args)...);
+		}
 
-        template<typename T>
-        bool HasComponent() const {
-            return m_Scene->m_Registry.all_of<T>(m_Entity);
-        }
+		template<typename T, typename... Args>
+		T& AddOrReplaceComponent(Args&&... args) {
+			return m_Scene->m_Registry.emplace_or_replace<T>(
+				m_Entity, std::forward<Args>(args)...);
+		}
 
-        operator bool() const {
-            return m_Entity != entt::null;
-        }
+		template<typename T>
+		void RemoveComponent() {
+			m_Scene->m_Registry.remove<T>(m_Entity);
+		}
 
-    private:
-        entt::entity m_Entity { entt::null };
-        Scene* m_Scene = nullptr;
+		template<typename T>
+		T& GetComponent() {
+			return m_Scene->m_Registry.get<T>(m_Entity);
+		}
 
-        friend class Scene;
-    };
+		template<typename T>
+		bool HasComponent() const {
+			return m_Scene->m_Registry.all_of<T>(m_Entity);
+		}
+
+		operator bool() const {
+			return m_Entity != entt::null;
+		}
+
+	private:
+		entt::entity m_Entity{entt::null};
+		Scene* m_Scene = nullptr;
+
+		friend class Scene;
+	};
 }
